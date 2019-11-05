e378f8955d8f144b15594599726968f12616c1c7
Markus <5563643+MarkusGeiger@users.noreply.github.com>
2019-11-05 01:35:14 +0100

    Implement App.Metrics QuickStart example code

---
+++ Experimental.AppMetrics/Controllers/MetricsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Experimental.AppMetrics.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Experimental.AppMetrics.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class MetricsController : ControllerBase
+  {
+    private readonly ILogger<MetricsController> _logger;
+    private readonly Metrics _metrics;
+
+    public MetricsController(ILogger<MetricsController> logger, Metrics metricsService)
+    {
+      _logger = logger;
+      _metrics = metricsService;
+    }
+
+    [HttpGet("increment")]
+    public IActionResult GetIncrement()
+    {
+      _metrics.IncrementCounter();
+      return Ok();
+    }
+
+    [HttpGet("snapshot")]
+    public async Task<IActionResult> GetSnapshot()
+    {
+      await _metrics.GetSnapshotAsync();
+      return Ok();
+    }
+  }
+}
---
+++ Experimental.AppMetrics/Services/Metrics.cs
@@ -0,0 +1,53 @@
+﻿using App.Metrics;
+using App.Metrics.Counter;
+using App.Metrics.Scheduling;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experimental.AppMetrics.Services
+{
+  public class Metrics
+  {
+    private readonly ILogger<Metrics> _logger;
+    private readonly IMetricsRoot _metrics;
+    private readonly CounterOptions _counter;
+
+    public Metrics(ILogger<Metrics> logger)
+    {
+      _logger = logger;
+      _metrics = new MetricsBuilder()
+        .Report.ToConsole()
+        .Build();
+      _counter = new CounterOptions { Name = "my_counter" };
+      _logger.LogDebug("Metrics built!");
+      var scheduler = new AppMetricsTaskScheduler(
+        TimeSpan.FromSeconds(3),
+        async () =>
+        {
+          await Task.WhenAll(_metrics.ReportRunner.RunAllAsync());
+        });
+      scheduler.Start();
+    }
+
+    public void IncrementCounter()
+    {
+      _metrics.Measure.Counter.Increment(_counter);
+    }
+
+    public async Task GetSnapshotAsync()
+    {
+      var snapshot = _metrics.Snapshot.Get();
+      using (var stream = new MemoryStream())
+      {
+        await _metrics.DefaultOutputMetricsFormatter.WriteAsync(stream, snapshot);
+        await _metrics.DefaultOutputEnvFormatter.WriteAsync(stream, _metrics.EnvironmentInfo);
+
+        var result = Encoding.UTF8.GetString(stream.ToArray());
+        _logger.LogInformation(result);
+      }
+    }
+  }
+}
